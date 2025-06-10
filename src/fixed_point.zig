7814830fb853aed9f7f522a8c2c2ae6feea4c5d6
Parker Liu <liuchangsheng30@gmail.com>
2025-06-10 19:59:23 +0800

    Change alg function to alg lambda for modify state of reduce
    The alg lambda can return with Error.

--- src/fixed_point.zig
+++ src/fixed_point.zig
@@ -4,6 +4,7 @@ const semi_grp = @import("semigroup.zig");
 const monoid = @import("monoid.zig");
 const functor = @import("functor.zig");
 const testu = @import("test_utils.zig");
+const maybe = @import("maybe.zig");
 const arraym = @import("array_monad.zig");
 
 const testing = std.testing;
@@ -17,9 +18,11 @@ const MapLamRetType = base.MapLamRetType;
 
 const FMapMode = base.FMapMode;
 const MapFnKind = base.MapFnKind;
+const mapFnToLam = base.mapFnToLam;
 const isMapRef = base.isMapRef;
 const isInplaceMap = base.isInplaceMap;
 const isErrorUnionOrVal = base.isErrorUnionOrVal;
+const maybe_error = maybe.maybe_error;
 
 const Monoid = monoid.Monoid;
 const Functor = functor.Functor;
@@ -36,26 +39,60 @@ pub fn Fix(comptime in_cfg: anytype, comptime F: TCtor) type {
         const Self = @This();
         pub const cfg = in_cfg;
         pub const FunF = F;
+        pub const Error = Functor(F).InstanceImpl.Error;
     };
 }
 
 /// A alg function type, just is
 /// `type Alg f a = f a -> a`
 /// in Haskell
-pub fn Alg(comptime F: TCtor, comptime A: type) type {
-    return *const fn (F(A)) A;
+pub fn Alg(comptime F: TCtor, comptime A: type, comptime E: ?type) type {
+    const EA = if (E == null) A else E.?!A;
+    return *const fn (F(A)) EA;
 }
 
-fn CataAlgLam(comptime cfg: anytype, comptime F: TCtor, comptime A: type) type {
+fn CataAlgLam(
+    comptime cfg: anytype,
+    comptime AlgType: type,
+    comptime F: TCtor,
+    comptime A: type,
+    comptime E: ?type,
+) type {
     return struct {
-        functor_impl: *const Functor(F).InstanceImpl,
-        fix_alg: Alg(F, A),
+        functor_impl: *const FImpl,
+        // alg lambda with function: Alg(F, A, E),
+        alg: AlgType,
 
         const Self = @This();
-        pub fn call(self: *const Self, fix: Fix(cfg, F)) A {
-            return self.fix_alg(self.functor_impl.fmapLam(.NewValMap, self, fix.fixed.*));
+        const FImpl = Functor(F).InstanceImpl;
+        const Error = maybe_error.mappend(E, FImpl.Error);
+        const EA = if (Error == null) A else Error.?!A;
+        pub fn call(self: *const Self, fix: Fix(cfg, F)) EA {
+            if (Error == null) {
+                return self.alg.call(self.functor_impl.fmapLam(.NewValMap, self, fix.fixed.*));
+            } else {
+                return self.alg.call(try self.functor_impl.fmapLam(.NewValMap, self, fix.fixed.*));
+            }
         }
     };
+}
+
+fn CataRetType(comptime FImplPtr: type, comptime AlgType: type, comptime FixType: type) type {
+    const AlgInType = MapLamInType(AlgType);
+    const AlgRetType = MapLamRetType(AlgType);
+    const has_err, const A = isErrorUnionOrVal(AlgRetType);
+    const E: ?type = if (has_err) @typeInfo(AlgRetType).error_union.error_set else null;
+
+    if (@typeInfo(FImplPtr) != .pointer) {
+        @compileError("Expect FImplPtr is *const Functor(F).InstanceImpl, found `" ++ @typeName(FImplPtr) ++ "`");
+    }
+    const F = std.meta.Child(FImplPtr).F;
+    std.debug.assert(F(A) == AlgInType);
+    std.debug.assert(Fix(FixType.cfg, F) == FixType);
+
+    const Error = maybe_error.mappend(E, FixType.Error);
+    const EA = if (Error == null) A else Error.?!A;
+    return EA;
 }
 
 /// A cata function, just is
@@ -64,18 +101,28 @@ fn CataAlgLam(comptime cfg: anytype, comptime F: TCtor, comptime A: type) type {
 /// cata alg = alg . fmap (cata alg) . unFix
 /// ```
 /// in Haskell
-/// The type of fix parameter is Fix(cfg, F)
+/// type of parameter f_impl is: *const Functor(F).InstanceImpl,
+/// type of parameter alg is lambda with function: Alg(F, A, E),
+/// type of parameter fix is: Fix(cfg, F)
 pub fn cata(
-    comptime F: TCtor,
-    comptime A: type,
-    f_impl: *const Functor(F).InstanceImpl,
-    alg: Alg(F, A),
-    fix: anytype,
-) A {
+    f_impl: anytype, // type is *const Functor(F).InstanceImpl,
+    alg: anytype, // type is lambda with function: Alg(F, A, E),
+    fix: anytype, // type is Fix(cfg, F)
+) CataRetType(@TypeOf(f_impl), @TypeOf(alg), @TypeOf(fix)) {
+    const FImplPtr = @TypeOf(f_impl);
+    const AlgType = @TypeOf(alg);
     const FixType = @TypeOf(fix);
-    std.debug.assert(Fix(FixType.cfg, F) == FixType);
-    const cata_alg = CataAlgLam(FixType.cfg, F, A){ .functor_impl = f_impl, .fix_alg = alg };
-    return alg(f_impl.fmapLam(.NewValMap, cata_alg, fix.fixed.*));
+    const RetType = CataRetType(FImplPtr, AlgType, FixType);
+    const has_err, const A = isErrorUnionOrVal(RetType);
+    const F = FixType.FunF;
+    const E: ?type = if (has_err) @typeInfo(RetType).error_union.error_set else null;
+    const cata_alg = CataAlgLam(FixType.cfg, AlgType, F, A, E){ .functor_impl = f_impl, .alg = alg };
+
+    if (E == null) {
+        return alg.call(f_impl.fmapLam(.NewValMap, cata_alg, fix.fixed.*));
+    } else {
+        return alg.call(try f_impl.fmapLam(.NewValMap, cata_alg, fix.fixed.*));
+    }
 }
 
 fn SimpleExprF(comptime X: type) type {
@@ -229,6 +276,51 @@ fn evalSimpleExprF(exprf: SimpleExprF(i32)) i32 {
     };
 }
 
+const ArrayList = std.ArrayList;
+
+const ShowSimpleExprFLam = struct {
+    show_str_ptr: *ArrayList(u8),
+
+    const Self = @This();
+    pub fn call(self: *const Self, exprf: SimpleExprF(ArrayList(u8))) !ArrayList(u8) {
+        var show_str_ptr = self.show_str_ptr;
+        switch (exprf) {
+            .liti => |int| {
+                const fmt_str = " {d} ";
+                const count: usize = @intCast(std.fmt.count(fmt_str, .{int}));
+                const buf = try show_str_ptr.addManyAsSlice(count);
+                _ = std.fmt.bufPrint(buf, fmt_str, .{int}) catch unreachable;
+                return show_str_ptr.*;
+            },
+            .add => |tuple| {
+                const rhs_start = tuple[0].items.len;
+                const rhs_end = tuple[1].items.len;
+                const rhs_len = rhs_end - rhs_start;
+                const fmt_str = " + ";
+                const count: usize = @intCast(std.fmt.count(fmt_str, .{}));
+                _ = try show_str_ptr.addManyAsSlice(count);
+                // copy right hand operand to rightest of new str
+                const new_len = show_str_ptr.items.len;
+                std.mem.copyBackwards(
+                    u8,
+                    show_str_ptr.items[new_len - rhs_len .. new_len],
+                    show_str_ptr.items[rhs_start..rhs_end],
+                );
+                const buf = show_str_ptr.items[rhs_start..][0..count];
+                _ = std.fmt.bufPrint(buf, fmt_str, .{}) catch unreachable;
+                return show_str_ptr.*;
+            },
+        }
+    }
+};
+
+fn printSimpleExprF(exprf: SimpleExprF(void)) !void {
+    return switch (exprf) {
+        .liti => |int| std.debug.print(" {d} ", .{int}),
+        .add => std.debug.print(" + ", .{}),
+    };
+}
+
 test "Fix(SimpleExprF) cata function" {
     const allocator = testing.allocator;
     const cfg = base.getDefaultBaseCfg(allocator);
@@ -243,6 +335,15 @@ test "Fix(SimpleExprF) cata function" {
         try liftLiti(13),
     );
     defer deinitSimpleExpr(cfg, expr);
-    const result = cata(SimpleExprF, i32, &exprf_functor, &evalSimpleExprF, expr);
+    const result = cata(&exprf_functor, mapFnToLam(evalSimpleExprF), expr);
     try testing.expectEqual(92, result);
+
+    var show_array = ArrayList(u8).init(allocator);
+    const show_exprf_lam = ShowSimpleExprFLam{ .show_str_ptr = &show_array };
+    defer show_exprf_lam.show_str_ptr.*.deinit();
+    const show_str = try cata(&exprf_functor, show_exprf_lam, expr);
+    _ = show_str;
+    try testing.expectEqualSlices(u8, " 42  +  37  +  13 ", show_array.items);
+
+    try cata(&exprf_functor, mapFnToLam(printSimpleExprF), expr);
 }
